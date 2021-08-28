98995303f8f766c9c02a57432e19b7bbffd521a6
Julie Vandard <julie.vandard@hey.com>
2021-08-28 16:16:52 +0200

    Component Skills animations down & up ok

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -5,15 +5,15 @@ import './styles.scss';
 export default function Skills() {
   return (
     <div className="skills-container">
-        <div className="skills-logos skills-logos_html"></div>
-        <div className="skills-logos skills-logos_css"></div>
-        <div className="skills-logos skills-logos_js"></div>
-        <div className="skills-logos skills-logos_sass"></div>
-        <div className="skills-logos skills-logos_react"></div>
-        <div className="skills-logos skills-logos_node"></div>
-        <div className="skills-logos skills-logos_express"></div>
-        <div className="skills-logos skills-logos_ejs"></div>
-        <div className="skills-logos skills-logos_postgres"></div>
+        <div className="skills-logos skills-logos_html bounce-down"></div>
+        <div className="skills-logos skills-logos_css bounce-up"></div>
+        <div className="skills-logos skills-logos_js bounce-down"></div>
+        <div className="skills-logos skills-logos_sass bounce-up"></div>
+        <div className="skills-logos skills-logos_react bounce-down"></div>
+        <div className="skills-logos skills-logos_node bounce-up"></div>
+        <div className="skills-logos skills-logos_express bounce-down"></div>
+        <div className="skills-logos skills-logos_ejs bounce-up"></div>
+        <div className="skills-logos skills-logos_postgres bounce-down"></div>
         <footer className="skills-footer">#Compétences</footer>
     </div>
   );
