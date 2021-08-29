db33e421f30b3c66599d263f0d606b875ebefef7
Julie Vandard <julie.vandard@hey.com>
2021-08-29 15:38:24 +0200

    Component Footer set up ok

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,16 +6,18 @@ export default function Footer() {
   return (
     <div className="footer-container">
     <section className="contacts-container">
-        <div className="coordonnees">
-            <p className="contact">julie.vandard@hey.com</p>
-            <p className="contact">06 76 07 07 06</p>
-        </div>
-        <div className="socials">
-            <a href="https://www.linkedin.com/in/julie-vandard/" target="_blank" rel="noreferrer">Mon profil LinkedIn</a>
-            <a href="https://github.com/juliedev-web" target="_blank" rel="noreferrer">Mon profil Github</a>
-            <a href="https://twitter.com/jvgazouille" target="_blank" rel="noreferrer">Mon compte twitter</a>
-            <a href="https://lesmainsdanslecode.com/" target="_blank" rel="noreferrer">Mon blog sur ma reconversion</a>
-        </div>
+        <ul className="coordonnees">
+            <li className="contact">julie.vandard@hey.com</li>
+            <li className="contact">06 76 07 07 06</li>
+        </ul>
+        <ul className="socials">
+            <li><a href="https://www.linkedin.com/in/julie-vandard/" target="_blank" rel="noreferrer">Mon profil LinkedIn</a></li>
+            <li> <a href="https://github.com/juliedev-web" target="_blank" rel="noreferrer">Mon profil Github</a></li>
+         </ul>
+         <ul>   
+            <li><a href="https://twitter.com/jvgazouille" target="_blank" rel="noreferrer">Mon compte twitter</a></li>
+            <li><a href="https://lesmainsdanslecode.com/" target="_blank" rel="noreferrer">Mon blog sur ma reconversion</a></li>
+        </ul>
     </section>
         <footer className="footer-footer">#Me contacter</footer>
     </div>
