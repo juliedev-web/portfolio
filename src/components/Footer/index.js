3d36d0fa613ec6e734db48c9d1b97081289d46c0
Julie Vandard <julie.vandard@hey.com>
2021-08-29 16:06:10 +0200

    Component Footer with icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,47 @@
 import React from 'react';
 
+import {
+  faEnvelopeSquare,
+  faPhoneSquare,
+  faBlog
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faLinkedin,
+  faGithubSquare,
+  faTwitterSquare,
+} from '@fortawesome/free-brands-svg-icons';
+
 import './styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Footer() {
   return (
     <div className="footer-container">
     <section className="contacts-container">
         <ul className="coordonnees">
-            <li className="contact">julie.vandard@hey.com</li>
-            <li className="contact">06 76 07 07 06</li>
+            <li className="contact">
+            <FontAwesomeIcon icon={faEnvelopeSquare} className="fa-icon" 
+            />julie.vandard@hey.com</li>
+            <li className="contact">
+            <FontAwesomeIcon icon={faPhoneSquare} className="fa-icon" 
+            /> 06 76 07 07 06</li>
         </ul>
         <ul className="socials">
-            <li><a href="https://www.linkedin.com/in/julie-vandard/" target="_blank" rel="noreferrer">Mon profil LinkedIn</a></li>
-            <li> <a href="https://github.com/juliedev-web" target="_blank" rel="noreferrer">Mon profil Github</a></li>
+            <li><a href="https://www.linkedin.com/in/julie-vandard/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} className="fa-icon" 
+            />Mon profil LinkedIn</a></li>
+            <li><a href="https://github.com/juliedev-web" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faGithubSquare} className="fa-icon" 
+            />Mon profil Github</a></li>
          </ul>
-         <ul>   
-            <li><a href="https://twitter.com/jvgazouille" target="_blank" rel="noreferrer">Mon compte twitter</a></li>
-            <li><a href="https://lesmainsdanslecode.com/" target="_blank" rel="noreferrer">Mon blog sur ma reconversion</a></li>
+         <ul className="socials">   
+            <li><a href="https://twitter.com/jvgazouille" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faTwitterSquare} className="fa-icon" 
+            />Mon compte twitter</a></li>
+            <li><a href="https://lesmainsdanslecode.com/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faBlog} className="fa-icon" 
+            />Mon blog sur ma reconversion</a></li>
         </ul>
     </section>
         <footer className="footer-footer">#Me contacter</footer>
