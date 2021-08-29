5c903f325f3b8360d209c08011b9c64f06d10ff2
Julie Vandard <julie.vandard@hey.com>
2021-08-29 17:47:57 +0200

    Title ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,20 @@ import './styles.scss';
 export default function Header () {
   return (
     <div className="header-container">
+      <section className="titles">
+        <div className="titles-name">
+          <p><span>Julie</span> Vandard</p>
+        </div>
+        <div className="titles-job">
+          <p><span>#</span>Développeuse <span>web frontend</span></p>
+        </div>
+      </section>
         <nav className="nav-container">
-          <ul className="links">
-            <li className="links links_parcours"><a href="#parcours" className="a_parcours">#Parcours</a></li>
-            <li className="links links_skills"><a href="#skills">#Compétences</a></li>
-            <li className="links links_projects"><a href="#projects">#Projets</a></li>
-            <li className="links links_contact"><a href="#contact">#Me contacter</a></li>
+          <ul className="sections">
+            <li className="sections sections_parcours"><p className="p_parcours">#Parcours</p></li>
+            <li className="sections sections_skills"><p>#Compétences</p></li>
+            <li className="sections sections_projects"><p>#Projets</p></li>
+            <li className="sections sections_contact"><p>#Me contacter</p></li>
           </ul>
         </nav>
     </div>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles/styles.scss';
 //components
 import App from './components/App';
 
-const rootReactElement = <App />;
+const rootReactElement = <App />
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const target = document.getElementById('root');
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
