ad299515cb5b6c92080f6bb00675bb76c228857b
Julie Vandard <julie.vandard@hey.com>
2021-08-29 20:14:00 +0200

    Responsive design

--- src/components/Parcours/index.js
+++ src/components/Parcours/index.js
@@ -9,11 +9,10 @@ export default function Parcours() {
             <article>
                 <p>Après dix-huit années dans le journalisme, dont quinze dans la mise en page, la relecture et la correction, j’ai décidé de me reconvertir et de devenir développeuse web frontend.</p>
                 <p>J’ai suivi une formation intensive chez <a href="https://oclock.io/" target="_blank">O’Clock </a>durant cinq mois pour me reconvertir.</p>
-                <p><span>(Photo by <a href="https://unsplash.com/photos/95YRwf6CNw8">Clément Hélardot on Unsplash</a>  )</span></p>
+                <p><span>(Photo by <a href="https://unsplash.com/photos/95YRwf6CNw8" target="_blank" rel="noreferrer">Clément Hélardot on Unsplash</a>  )</span></p>
             </article>
             <footer className="parcours-footer">#Mon parcours</footer>
         </section>
-        
         <div className="illustration"></div>
     </div>
   );
