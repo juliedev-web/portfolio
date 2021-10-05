9961fc06c5605038dae3934e2a209dec8c6b0ed0
Julie Vandard <julie.vandard@hey.com>
2021-10-05 17:23:15 +0200

    SideProjets add Apotheose Engrainons-nous

--- src/components/SideProjects/index.js
+++ src/components/SideProjects/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './styles.scss';
 
+import apotheose from '../../assets/images/projects/apotheose.png';
 import pancakes from '../../assets/images/projects/pancakes.png';
 import uke from '../../assets/images/projects/ukeproject.png';
 import portfolio from '../../assets/images/projects/portfolio.png';
@@ -12,18 +13,24 @@ export default function SideProjects() {
     <div className="sideprojects-container">
       <header className="sideprojects-header">#Projets</header>
         <section className="cards-container">
+        <article className="card">
+            <h3 className="card-title">Engrainons-nous</h3>
+            <img className="card-img" src={apotheose} alt="capture d'écran du site"/>
+            <p className="card-description">Engrainons-nous est le projet d'Apothéose réalisé en fin de formation O'Clock. L'équipe 3 dev front (dont moi) et 2 dev backs. Projet réalisé en méthode agile, avec des sprints de 2 semaines from scratch. Sur ce projet j'étais Scrum master et dev front. (Sur YouTube, suite à un soucis de son, montez le volume)</p>
+            <a href="https://youtu.be/PHYfwht2dJU?t=568" target="_blank" rel="noreferrer" className="card-link card-link_FEM">La démo live sur YouTube</a>
+            <a href="https://github.com/juliedev-web/Apo-Engrainons-nous" target="_blank" rel="noreferrer" className="card-link">Le lien Github</a>
+          </article>
           <article className="card">
             <h3 className="card-title">Pimp my pancakes</h3>
             <img className="card-img" src={pancakes} alt="capture d'écran du site"/>
-            <p className="card-description">L'objectif est de reprendre un challenge réalisé durant ma formation chez O'Clock pour l'améliorer : nouvelle mise en page avec sections alternant fond coloré ou des images de fond, nouvelle palette de couleurs et polices, ajout d'un header et d'un footer. Un projet pour réviser et revisiter !</p>
+            <p className="card-description">L'objectif était de reprendre un challenge réalisé durant ma formation chez O'Clock pour l'améliorer : nouvelle mise en page avec sections alternant fond coloré ou des images de fond, nouvelle palette de couleurs et polices, ajout d'un header et d'un footer. Un projet revisité pour réviser !</p>
             <a href="https://pimp-my-pancakes.netlify.app/" target="_blank" rel="noreferrer" className="card-link card-link_FEM">Le projet en live</a>
             <a href="https://github.com/juliedev-web/PimpMyPancakes" target="_blank" rel="noreferrer" className="card-link">Le lien Github</a>
           </article>
           <article className="card">
             <h3 className="card-title">The ukulele project</h3>
             <img className="card-img" src={uke} alt="capture d'écran du site" />
-            <p className="card-description">Le but est de pouvoir m'exercer sur les notions apprises en formation, et de monter en compétences. 
-            Une seconde version avec le même contenu sera réalisée avec une mise en page plus moderne, et des choix techniques différents.</p>
+            <p className="card-description">Au départ, ce n'était qu'un dessin d'ukulélé en CSS. J'ai eu envie d'en faire un site complet avec ce que je peux "enseigner" quand je donne des cours d'ukulélé. Ici le design coloré est assumé. J'étudie aussi la possibilité d'ajouter une API pour générer des accords (en cours).</p>
             <a href="https://theukuleleproject.vercel.app" target="_blank" rel="noreferrer" className="card-link">Le projet en live</a>
             <a href="https://github.com/juliedev-web/theukuleleproject" target="_blank" rel="noreferrer" className="card-link">Le lien Github</a>
           </article>
